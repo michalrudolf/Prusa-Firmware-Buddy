21f79b0400a260ac8eb0d667333982175a9345b1
Michal Rudolf <michal.rudolf23@gmail.com>
2020-04-02 11:06:46 +0200

    http tests implemented without testing the response

--- utils/test_http_server/http_server.py
+++ utils/test_http_server/http_server.py
@@ -1,65 +1,137 @@
+import ipaddress
 import requests
 import logging
 import sys
 import time
 from datetime import datetime
 
-TELEMETRY_URL = ""
-GCODE_URL = ""
-ADMIN_URL = ""
+# admin_valid = '{"connect_ip":"10.24.230.10","connect_key":"01234567890123456789","connect_name":"P3D"}'
+# admin_not_valid_ip = '{"connect_ip":"1000.24.230.10","connect_key":"11234567890123456789","connect_name":"PRUSA3D"}'
+# admin_longer_str = '{"connect_ip":"10.24.230.10","connect_key":"11114567890123456789123456","connect_name":"PRUSA3D01234567890123456789"}'
 
-def main():
+# global variables
+IP_ADDR = ""
+test_cnt = 0
+tests_off = 0
+json_tests = {}
 
-    global TELEMETRY_URL, GCODE_URL, ADMIN_URL
+# gcode structures
+G_AUTOHOME = {"command":"G28"}
+G_MOVES = {"command":"G1 X50 Y100", "command":"G1 X100 Y50", "command":"G1 X60 Y90", "command":"G1 X100 Y50", "command":"G1 X70 Y80"}
 
-    args_count = len(sys.argv)
-    args_list = sys.argv
-    test_valid = 0
-    my_printer_ip = ''
+def init(ip_addr):
+    global json_tests, test_cnt, IP_ADDR
+
+    # set ip address
+    IP_ADDR = 'http://' + ip_addr
+
+    # load JSON tests
+    json_file = open("/server_tests/tests.json", "r")
+    json_obj = json.load(json_file)
+    json_file.close()
+
+    json_tests = json_obj['tests']
+
+    # disable switched off tests
+    for test in json_tests:
+        if "switch" in test:
+            if "off" in test["switch"]:
+                json_tests.remove(test)
+    
+    if len(json_tests) == 0:
+        print("All tests switched off")
+    
+    test_cnt = len(json_tests)
 
     # logging init
     logging.basicConfig(filename='server_tests/errors.log', filemode='w', level=logging.ERROR)
 
-    # check argument
+def main():
+    global test_cnt
+    args_count = len(sys.argv)
+    args_list = sys.argv
+
+    # check script argument
     if args_count == 2:
-        my_printer_ip = args_list[1]
+        try:
+            ip = ipaddress.ip_address(sys.argv[1])    
+        except ValueError:
+            print('Address is invalid: %s' % sys.argv[1])
+            return
+        init(args_list[1])
     else:
         print("\nRun again and add argument IP of tested printer\nexample: python HTTP_server_tests.py 10.24.230.10\n")
         return
 
-    gcode_autohome = '{"command":"G28"}'
-    gcode_moving = '{"command":"G1 X50 Y100", "command":"G1 X100 Y50", "command":"G1 X60 Y90", "command":"G1 X100 Y50", "command":"G1 X70 Y80"}'
-    gcode_name_mistake = '{"comman":"G28"}'
-    gcode_code_mistake = '{"command":"G"}'
-
-    admin_valid = '{"connect_ip":"10.24.230.10","connect_key":"01234567890123456789","connect_name":"P3D"}'
-    admin_not_valid_ip = '{"connect_ip":"1000.24.230.10","connect_key":"11234567890123456789","connect_name":"PRUSA3D"}'
-    admin_longer_str = '{"connect_ip":"10.24.230.10","connect_key":"11114567890123456789123456","connect_name":"PRUSA3D01234567890123456789"}'
-
-    post_header = "'Content-type: application/json'"
-
-    TELEMETRY_URL = 'http://' + my_printer_ip + '/api/telemetry'
-    GCODE_URL = 'http://' + my_printer_ip + '/api/g-code'
-    ADMIN_URL = 'http://' + my_printer_ip + '/admin.html'
-
     while 1:
-        
+
         test_get_telemetry()
-        test_post_gcode()
-        print("sleep")
+        if test_cnt != 0:
+            test_loop()
+
         time.sleep(1)
 
+def test_loop():
+    global json_tests
+
+    
+    name = json_tests[test_curr]['name']
+
+    result_dic = send_request(name)
+    if len(result_dic) != 0:
+        test_response(result_dic)
+
+    if (test_curr + 1) >= test_cnt :
+            test_curr = 0
+            off_test_cnt = 0
+        else:
+            test_curr += 1
+
+def send_request(name):
+    global test_curr, test_cnt, json_tests, IP_ADDR
+    
+    test_header = json_tests[test_curr]['request']['header']
+    headers = {}
+    res_dic = {}
+    ip_addr = IP_ADDR + json_tests[test_curr]['uri']
+
+    if 'token' in test_header:
+        headers.update({"Printer-Token" : str(test_header['token'])})
+
+    if 'GET' in test_header['method']:
+        if len(headers) != 0:
+            response = requests.get(url = ip_addr, headers = headers)
+        else:
+            response = requests.get(url = ip_addr)
+    elif 'POST' in test_header['method']:
+        if len(headers) != 0:
+            response = requests.post(url = ip_addr, headers = headers, json = json_tests['request']['body'])
+        else:
+            response = requests.post(url = ip_addr, json = json_tests['request']['body'])
+    else:
+        test_failed(str(json_tests[test_curr]), name + " has unsupported test method...")
+        return {}
+
+    try:
+        res_dic = response.json()
+    except ValueError:
+        test_failed(str(res_dic), "Response parsing to JSON")
+        return {}
+    
+    return res_dic
+
+def test_response(result_dic):
+    pass
+
 def test_get_telemetry():
-    response = requests.get(TELEMETRY_URL)
-    res_dic = response.json()
-    if not isinstance(res_dic, dict):
-        print("Couldn't decode json structure of Telemetry")
+    response = requests.get(IP_ADDR + "/api/telemetry")
+    try:
+        res_dic = response.json()
+    except ValueError:
+        test_failed(str(res_dic), "Telemetry parsing to JSON")
         return
 
     test_telemetry_response(res_dic)
-
-def test_post_gcode():
-    pass
 
 def test_telemetry_response(response_dic):
     
