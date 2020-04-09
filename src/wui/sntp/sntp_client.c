e1b4f9dcd4ffce85987d72bbc7e9a274766d2497
Michal Rudolf <michal.rudolf23@gmail.com>
2020-04-09 17:06:30 +0200

    get time and get date funcs

--- src/wui/sntp/sntp_client.c
+++ src/wui/sntp/sntp_client.c
@@ -1,5 +1,6 @@
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "lwip.h"
 #include "sntp.h"
 #include "sntp_client.h"
@@ -14,7 +15,9 @@
 //    tv->tv_usec = (suseconds_t)( t % 1000 ) * 1000;  // get remaining
 //    return 0;
 //}
-
+static char sys_time[10];
+static char sys_date[10];
+static bool sntp_time_init;
 static ip_addr_t ntp_server;  // testing ntp server located in Prague
 
 void sntp_set_system_time(uint32_t sec)
@@ -37,10 +40,41 @@ void sntp_set_system_time(uint32_t sec)
 
   HAL_RTC_SetTime(&hrtc, &currTime, RTC_FORMAT_BIN);
   HAL_RTC_SetDate(&hrtc, &currDate, RTC_FORMAT_BIN);
+
+  sntp_time_init = true;
+}
+
+const char * sntp_get_system_time(void){
+
+  if(sntp_time_init){
+    RTC_TimeTypeDef currTime;
+
+    HAL_RTC_GetTime(&hrtc, &currTime, RTC_FORMAT_BIN);
+
+    snprintf(sys_time, 10, "%02d:%02d:%02d", currTime.Hours, currTime.Minutes, currTime.Seconds);
+  } else {
+    strcpy(sys_time, "N/A");
+  }
+  return sys_time;
+}
+
+const char * sntp_get_system_date(void){
+
+  if(sntp_time_init){
+    RTC_DateTypeDef currDate;
+
+    HAL_RTC_GetDate(&hrtc, &currDate, RTC_FORMAT_BIN);
+
+    snprintf(sys_date, 10, "%02d:%02d:%02d", currDate.Date, currDate.Month, currDate.Year);
+  } else {
+    strcpy(sys_date, "N/A");
+  }
+  return sys_date;
 }
 
 void sntp_client_init(void)
 {
+  sntp_time_init = false;
   sntp_setoperatingmode(SNTP_OPMODE_POLL);
 
   //uint8_t netif_type = eeprom_get_var(EEVAR_LAN_FLAG).ui8;
