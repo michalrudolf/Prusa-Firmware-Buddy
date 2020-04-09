ce97691a6df885d0bbbbd7e34a32c71600f1ea2f
Michal Rudolf <michal.rudolf23@gmail.com>
2020-04-09 15:26:41 +0200

    implement set_system_time fnc

--- src/wui/sntp/sntp_client.c
+++ src/wui/sntp/sntp_client.c
@@ -3,6 +3,8 @@
 #include "lwip.h"
 #include "sntp.h"
 #include "sntp_client.h"
+#include "stm32f4xx_hal.h"
+#include "main.h" // for hrtc -> pointer to RTC storage structure
 
 //#define _gettimeofday(x, y) get_time_of_day(x, y)
 //int gettimeofday( struct timeval *tv, void *tzvp )
@@ -13,21 +15,28 @@
 //    return 0;
 //}
 
+static ip_addr_t ntp_server;  // testing ntp server located in Prague
 
-static ip_addr_t ntp_server;
 void sntp_set_system_time(uint32_t sec)
-{
-  //char buf[32];
-  //struct tm current_time_val;
-  //time_t current_time = (time_t)sec;
+{ 
+  RTC_TimeTypeDef currTime;
+  RTC_DateTypeDef currDate;
 
-  //localtime_s(&current_time_val, &current_time);
+  struct tm current_time_val;
+  time_t current_time = (time_t)sec;
 
-  //strftime(buf, sizeof(buf), "%d.%m.%Y %H:%M:%S", &current_time_val);
-  
-  uint32_t real_time = sec;
-  real_time++;
+  localtime_r(&current_time, &current_time_val);
 
+  currTime.Seconds = current_time_val.tm_sec;
+  currTime.Minutes = current_time_val.tm_min;
+  currTime.Hours = current_time_val.tm_hour;
+  currDate.Date = current_time_val.tm_mday;
+  currDate.Month = current_time_val.tm_mon;
+  currDate.Year = current_time_val.tm_year;
+  currDate.WeekDay = current_time_val.tm_wday;
+
+  HAL_RTC_SetTime(&hrtc, &currTime, RTC_FORMAT_BIN);
+  HAL_RTC_SetDate(&hrtc, &currDate, RTC_FORMAT_BIN);
 }
 
 void sntp_client_init(void)
@@ -39,7 +48,9 @@ void sntp_client_init(void)
   //if ((netif_type & LAN_EEFLG_TYPE) == 0){
   //  sntp_servermode_dhcp(1); /* get SNTP server via DHCP */
   //} else {
-    // in LwIP init netif_default is set to eth0
+
+    // In LwIP init netif_default is set to eth0, BUT \
+    // When DHCP is enabled, it supplies addrs after this is called -> set_server 0.0.0.0
     if(ipaddr_aton("195.113.144.201", &ntp_server)){
       sntp_setserver(0, &ntp_server);
     }
