dad8ab8539d1d5a699dcfe074eb560904e2458f6
joshymjose <joshymjose@gmail.com>
2020-04-23 17:12:33 +0200

    first api function done

--- src/wui/wui_api.h
+++ src/wui/wui_api.h
@@ -9,17 +9,45 @@
 #ifndef _WUI_API_H_
 #define _WUI_API_H_
 
+#include "lwip/netif.h"
+#include "eeprom.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef enum {
+    NETVAR_LAN_FLAGS,
+    NETVAR_HOSTNAME,
+    NETVAR_CONNECT_TOKEN,
+    NETVAR_LAN_IP4_ADDR,
+    NETVAR_LAN_IP4_MSK,
+    NETVAR_LAN_IP4_GW,
+    NETVAR_DNS1,
+    NETVAR_DNS2,
+    NETVAR_CONNECT_IP4,
+} NETVAR_t;
+
+#define NETVAR_MSK(n_id) ((uint16_t)1 << (n_id))
+#define NETVAR_STATIC_LAN_ADDRS \
+    (NETVAR_MSK(NETVAR_LAN_IP4_ADDR) | NETVAR_MSK(NETVAR_LAN_IP4_MSK) | NETVAR_MSK(NETVAR_LAN_IP4_GW))
+
 typedef struct {
+    uint8_t lan_flag;
+    char hostname[LAN_HOSTNAME_MAX_LEN + 1];
+    char connect_token[CONNECT_TOKEN_SIZE + 1];
+    ip4_addr_t connect_ip4;
+    ip4_addr_t lan_ip4_addr;
+    ip4_addr_t lan_ip4_msk;
+    ip4_addr_t lan_ip4_gw;
+    uint16_t set_flag;
+} ETH_Config_t;
 
-} wui_eth_params_s;
-
-/* \breif function to pass ethernet parameters for WUI
+/* \breif function to save Ethernt configuration to non-volatile memory
 */
-void get_eth_params(wui_eth_params_s eth_params);
+uint32_t eeprom_set_eth_params(ETH_Config_t *config_t);
+
+void load_ini_params(ETH_Config_t *config_t);
 
 #ifdef __cplusplus
 }
